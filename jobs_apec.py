a2293b952f0400c8b0848d170ec40f6c04b5c0e2
atchopba <atchopba@gmail.com>
2020-04-23 23:00:05 +0200

    Refactoring code

--- jobs_apec.py
+++ jobs_apec.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-__author__ = "Albin TCHOPBA"
-__copyright__ = "Copyright 2020 Albin TCHOPBA and contributors"
-__credits__ = ["Albin TCHOPBA and contributors"]
-__license__ = "GPL"
-__version__ = "3"
-__maintainer__ = "Albin TCHOPBA"
-__email__ = "Albin TCHOPBA <atchopba @ gmail dot com"
-__status__ = "Production"
+# __author__ = "Albin TCHOPBA"
+# __copyright__ = "Copyright 2020 Albin TCHOPBA and contributors"
+# __credits__ = ["Albin TCHOPBA and contributors"]
+# __license__ = "GPL"
+# __version__ = "3"
+# __maintainer__ = "Albin TCHOPBA"
+# __email__ = "Albin TCHOPBA <atchopba @ gmail dot com"
+# __status__ = "Production"
 
 # pour exploiter les requêtes
 from requests import post
